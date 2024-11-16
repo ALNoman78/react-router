24a057ec47b2166adeafe80d7ebfde7b67da6b7d
ALNoman78 <abdullaalno078@gmail.com>
2024-11-16 22:54:11 +0600

    react router context

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom"
+
 const Navbar = () => {
     return (
         <div className="text-center ">
             <h2 className="font-bold text-2xl text-center">Navbar</h2>
             <nav className="flex justify-center items-center gap-6 text-xl font-semibold">
-                <a href="/">Home</a>
-                <a href="/about">About</a>
-                <a href="/contact">Contact Us</a>
+                <Link to="/" className="btn btn-neutral">Home</Link>
+                <Link to = '/about' className="btn btn-neutral">About</Link>
+                <Link to = '/contact' className="btn btn-neutral">Contact Us</Link>
             </nav>
         </div>
     )
