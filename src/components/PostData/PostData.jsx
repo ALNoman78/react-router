703fa4e85349ce94e5eb245e3c16392ef85707a4
ALNoman78 <abdullaalno078@gmail.com>
2024-11-18 22:58:12 +0600

    loader cannt applied when i fetch data

--- src/components/PostData/PostData.jsx
+++ src/components/PostData/PostData.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
 
-const PostData = ({data}) => {
-    console.log(data)
-    const {title , body} = data
+const PostData = ({ data }) => {
+    // console.log(data)
+    const { title, body } = data
     return (
         <div className='border border-blue-400 rounded-xl p-5 m-4'>
+            
             <h2>Title : {title}</h2>
             <p className='w-[20rem] mx-auto'><small>Comment : {body}</small></p>
         </div>
@@ -12,7 +13,7 @@ const PostData = ({data}) => {
 }
 
 PostData.propTypes = {
-    data : PropTypes.object.isRequired,
+    data: PropTypes.object.isRequired,
 }
 
 export default PostData
